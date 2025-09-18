cbd4a5aa31a0e3e710db4facee7c143d42ae2fd4
Samuel-cyber-droid <samy21jul@gmail.com>
2025-09-18 12:46:03 -0600

    Primeros ejercicios

---
+++ ejemploedades.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    'Edad': [18, 25, 40, 60],
+    'Ingresos':[1000, 2500, 4000, 8000],
+    'Color':['Rojo', 'Azul', 'Verder', 'Rojo'],
+    'Clase':['A', 'B', 'C', 'D']
+})
